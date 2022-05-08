d6edade24ff27b8012780df12fcf670fcf7091d2
David Nieto <david.nieto@adevinta.com>
2022-05-08 02:46:42 +0200

    finish exercise

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,17 +5,21 @@ import * as React from 'react'
 
 function Greeting() {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  // const name = ''
+
+  const [name, setName] = React.useState('David Nieto')
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    const {value} = event.target
+    setName(value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
